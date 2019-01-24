ce969cadceffe536f83bfd6d4b64a4dcc08e60a2
Brandon Burke <brandonburke93@outlook.com>
2019-01-24 16:10:19 -0700

    Comment dealer methods

--- 21CardGame/Dealer.cs
+++ 21CardGame/Dealer.cs
@@ -8,8 +8,19 @@ namespace _21CardGame
 {
     class Dealer
     {
+        /// <summary>
+        /// Count of how many times the cards have been dealt out
+        /// </summary>
         private int dealNumber = 0;
+
+        /// <summary>
+        /// Full 52 card deck
+        /// </summary>
         private Deck cardDeck;
+
+        /// <summary>
+        /// The random 21 cards taken from the full deck
+        /// </summary>
         private List<Card> randomDeck;
 
         public Dealer()
@@ -18,16 +29,25 @@ namespace _21CardGame
             //randomDeck = cardDeck.Random21();
         }
 
+        /// <summary>
+        /// Deal the 21 cards out
+        /// </summary>
         public void Deal()
         {
 
         }
 
+        /// <summary>
+        /// Add cards back to the list in specific order
+        /// </summary>
         public void PickupCards()
         {
 
         }
 
+        /// <summary>
+        /// Reveal to the user the secret card they picked
+        /// </summary>
         public void RevealCard()
         {
 
