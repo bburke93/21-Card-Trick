424023031a3e852d5afd6892b7938759674f3532
sometingclever <middletonry@gmail.com>
2019-01-26 19:26:50 -0700

    Column.cs updated

--- 21CardGame/Column.cs
+++ 21CardGame/Column.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,19 @@ namespace _21CardGame
 {
     class Column
     {
+        private int id;
+        Card[] cardArr = new Card[7];
+
+        public void addCard(Card card)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if (cardArr[i] == null)
+                {
+                    cardArr[i] = card;
+                    break;
+                }
+            }
+        }
     }
 }
