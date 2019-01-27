bea653b6e17c17168a495968d7f3066821dd5324
alexpetersonWSU <46539045+alexpetersonWSU@users.noreply.github.com>
2019-01-26 18:54:10 -0800

    Add files via upload

--- 21CardGame/MainWindow.xaml.cs
+++ 21CardGame/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,10 +21,175 @@ namespace _21CardGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// indicates whether the player has selected a card
+        /// </summary>
+        private bool hasSelectedCard = false;
+
         public MainWindow()
         {
             InitializeComponent();
             
+        }
+
+        /// <summary>
+        /// What happens when the Start Button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Start_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Start_Button.Visibility = Visibility.Hidden;
+
+                //Dealer shuffles and selects 21 cards 
+
+                deal();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// deals the cards
+        /// </summary>
+        private void deal()
+        {
+            try
+            {
+                //call Dealer.deal()
+
+                //Deal cards to GUI using the info from the column class
+
+                //check if player has picked a card
+                if (hasSelectedCard)
+                    indicateColumn();
+                else
+                    pickCard();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Makes it so the player can indicate that they have selected a card
+        /// </summary>
+        private void pickCard()
+        {
+            try
+            {
+                pickedCardButton.Visibility = Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// what happens when the picked card button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pickedCardButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                pickedCardButton.Visibility = Visibility.Hidden;
+                hasSelectedCard = true;
+                indicateColumn();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// makes it so the player can choose a column
+        /// </summary>
+        private void indicateColumn()
+        {
+            try
+            {
+                buttoncolumn1.Visibility = Visibility.Visible;
+                buttoncolumn2.Visibility = Visibility.Visible;
+                buttoncolumn3.Visibility = Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// what happens when a column is selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttoncolumn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                buttoncolumn1.Visibility = Visibility.Hidden;
+                buttoncolumn2.Visibility = Visibility.Hidden;
+                buttoncolumn3.Visibility = Visibility.Hidden;
+
+                Button column = (Button)sender;
+
+                pickupCards();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// picks up the cards
+        /// </summary>
+        private void pickupCards()
+        {
+            try
+            {
+                //call Dealer.pickupCards() & pass in column.Tag //tags: 0, 1, 2
+
+                //GUI picks up cards
+
+                deal();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// handles errors
+        /// </summary>
+        /// <param name="sClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (System.Exception ex)
+            {
+                System.IO.File.AppendAllText(@"C:\Error.txt", Environment.NewLine + "HandleError Exception: " + ex.Message);
+            }
         }
     }
 }
