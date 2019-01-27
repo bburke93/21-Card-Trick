f889e99fbf89042c746fe1bd84ca1df2cbb3f7bd
sometingclever <middletonry@gmail.com>
2019-01-26 18:25:08 -0800

    Add files via upload

---
+++ Column.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21CardGame
+{
+    class Column
+    {
+        private int id;
+        Card[] cardArr = new Card[7];
+
+        public void addCard(Card card)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if (cardArr[i] == null)
+                {
+                    cardArr[i] = card;
+                    break;
+                }
+            }
+        }
+    }
+}
